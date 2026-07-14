610a5d4db6de4d7b5ade0971ef95decd8bc98184
Aslan Öztürk <aslan.ozturk998@gmail.com>
2026-07-14 04:52:50 +0300

    feat(validation): add data annotations to TaskItem model.
    - Make Title required and set max length to 100.
    
    - Set max lenght of Description to 500 to prevent large inputs.

--- Models/TaskItem.cs
+++ Models/TaskItem.cs
@@ -1,11 +1,17 @@
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace TaskManagmentAPI.Models
 {
     public class TaskItem
     {
         public int Id {get;set;}
+
+        [Required(ErrorMessage ="Tasks must contain a title.")]
+        [MaxLength(100, ErrorMessage ="Title can be max 100 characters.")]
         public string Title {get; set;}
+
+        [MaxLength(500)]
         public string Description {get;set;}
         public bool IsDone {get;set;}
         public DateTime CreateDate {get;set;} = DateTime.Now; // Default created time.
